87ba5daaad1efa47f9b4205c7d665b41ebc54d2f
Adisaduke <rasakadisa.fcaib@gmail.com>
2026-04-05 09:16:37 +0100

    correct

--- src/predict.py
+++ src/predict.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     model, class_names = load_model()
 
     # CHANGE THIS PATH
-    image_path = "/content/drive/MyDrive/microstructure_project/processed/UHCS/test/spheroidite/Croppedmicrograph196.png"
+    image_path = "/content/drive/MyDrive/Colab Notebooks/microstructure_project/processed/UHCS/test/spheroidite/Croppedmicrograph536.png"
     if not os.path.exists(image_path):
         print("Image not found. Check path.")
     else:
